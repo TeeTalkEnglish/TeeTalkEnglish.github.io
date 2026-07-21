bec2f2d8812156ca72a0cb14b3416419391ebe3d
tnguyen2020 <thinguyenhoang.258@gmail.com>
2026-07-21 20:18:02 +0700

    add games.json and adsense.js file

---
+++ assets/js/adsense.js
@@ -0,0 +1,6 @@
+const script = document.createElement("script");
+script.async = true;
+script.src =
+  "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7908187227146035";
+script.crossOrigin = "anonymous";
+document.head.appendChild(script);
