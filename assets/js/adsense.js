653e7a225e7699ef6125cc658014d70466d9751c
tnguyen2020 <thinguyenhoang.258@gmail.com>
2026-07-21 20:32:44 +0700

    add adsense script to main page.

--- assets/js/adsense.js
+++ assets/js/adsense.js
@@ -1,6 +1,2 @@
-const script = document.createElement("script");
-script.async = true;
-script.src =
-  "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7908187227146035";
-script.crossOrigin = "anonymous";
-document.head.appendChild(script);
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7908187227146035"
+     crossorigin="anonymous"></script>
